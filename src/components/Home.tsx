48541680382d02a56f56cdbc81953b36f48b2a58
Scott Alton <scottaltonmusic@gmail.com>
2021-06-02 11:58:13 -0400

    Homepage styles added.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
@@ -6,6 +7,7 @@ const Home: React.FC = () => {
       <div className="hero-left">
         <h2>Spice Up Your Life</h2>
         <p>Bringing the world's finest herbs and spices to your doorstep.</p>
+        <Link to="/shop"><button className="btn btn-shop">Browse Store</button></Link>
       </div>
     </section>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Shop from './Shop';
+import Contact from './Contact';
 
 const Navbar: React.FC = () => {
   return (
     <Router>
       <nav>
-        <h1>Anise Spice Market</h1>
-
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -16,11 +15,30 @@ const Navbar: React.FC = () => {
           <li>
             <Link to="/shop">Shop</Link>
           </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <h1>Anise Spice Market</h1>
+
+        <ul>
+          <li>
+            <Link to="">
+              <i className="fa fa-search"></i>
+            </Link>
+          </li>
+          <li>
+            <Link to="">
+            <i className="fa fa-shopping-cart"></i>
+            </Link> 0
+          </li>
         </ul>
       </nav>
 
       <Switch>
         <Route exact path="/shop" component={Shop} />
+        <Route exact path="/contact" component={Contact} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
