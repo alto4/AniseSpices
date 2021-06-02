25cebc17ad07126755ab85cf27397cb15ef17f6f
Scott Alton <scottaltonmusic@gmail.com>
2021-06-02 12:39:25 -0400

    Layout and styles added to shop page. Cards mocked in for now.

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,7 +1,112 @@
 import React from 'react';
 
 const Shop: React.FC = () => {
-  return <div>Hello from Shop</div>;
+  return (
+    <section className="store-section">
+      <div className="container">
+        <h1><i className="fa fa-pepper-hot"></i> Shop Spices</h1>
+          
+        <h2>Our Favourites</h2>
+          <div className="items-container">
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+          </div>
+
+        <h2>All</h2>
+
+        <div className="items-container">
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+            <div className="item-card">
+              <h3>Oregano</h3>
+              <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/oregano-flakes-spoon-1296x728.jpg?w=1155&h=1528" alt="Dried oregano in a classy ceramic spoon." />
+              <div className="details">
+                <p>Price: $3.99</p>
+                <button className="btn btn-sm"><i className="fa fa-shopping-cart"></i> Add</button>
+                <button className="btn btn-sm"><i className="fa fa-list"></i> Details</button>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+  )
 };
 
 export default Shop;
