549a5b0fca0c467e8d5ab99d2c4fbd7a28fba98b
Scott Alton <scottaltonmusic@gmail.com>
2021-06-02 09:47:25 -0400

    File structure created and router added.

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-left">
+        <h2>Spice Up Your Life</h2>
+        <p>Bringing the world's finest herbs and spices to your doorstep.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Shop from './Shop';
+
+const Navbar = () => {
+  return (
+    <Router>
+      <nav>
+        <h1>Anise Spice Market</h1>
+
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/shop" component={Shop} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Navbar;
